306401e24dfa298aca8665625cbe5fc27aa842e9
Andy Herzog <asherzog19@gmail.com>
2017-01-06 11:43:29 -0700

    new user signup

---
+++ assets/js/signup.js
@@ -0,0 +1,20 @@
+$(() => {
+  $('form').submit((event) => {
+    event.preventDefault();
+    let user = {
+      name: $('#userName').val(),
+      email: $('#email').val(),
+      password: $('#password').val()
+    };
+    signUp(user)
+      .then(result => {
+        window.location = `/profile.html?id=${result.id}`;
+      }).catch(error => {
+        console.error(error);
+      });
+  });
+});
+
+function signUp(user) {
+  return $.post(`${local}/signup`, user)
+}
